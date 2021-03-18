029197a2dda6bc1c8bc9644fbce8d8e3ba501b54
Junaed Rayhan <junaedrayhan@gmail.com>
2021-03-19 01:41:37 +0600

    some changes

---
+++ src/components/Login/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB2Qz8SoItOtRptNoU9AK8VQ0arAgXniAA",
+    authDomain: "webgenesis-urban-riders.firebaseapp.com",
+    projectId: "webgenesis-urban-riders",
+    storageBucket: "webgenesis-urban-riders.appspot.com",
+    messagingSenderId: "532202656762",
+    appId: "1:532202656762:web:0614ec9ad254f7f9458a43"
+  };
+
+export default firebaseConfig;
